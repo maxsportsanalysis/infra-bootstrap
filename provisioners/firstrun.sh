5e9f8c19ce6b4dde40411d19adc2f582aea7de0f
Maxim Cilek <maxssportsanalysis@gmail.com>
2025-10-18 07:17:58 -0500

    Configured Workflow Packer Build

--- provisioners/firstrun.sh
+++ provisioners/firstrun.sh
@@ -21,8 +21,6 @@ KBEOF
    dpkg-reconfigure -f noninteractive keyboard-configuration
 fi
 
-wget -q https://releases.ubuntu.com/24.04/ubuntu-24.04.3-live-server-amd64.iso -O /var/www/html/pxe/ubuntu/24.04/ubuntu-24.04.3-live-server-amd64.iso
-
 # --- Cleanup ---
 rm -f /boot/firstrun.sh
 sed -i 's| systemd.run.*||g' /boot/cmdline.txt
