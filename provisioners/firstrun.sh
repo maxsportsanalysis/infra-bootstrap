5e22cf87f1a04d63a09bf3e43772244989b117f8
Maxim Cilek <maxssportsanalysis@gmail.com>
2025-11-05 02:27:59 -0600

    Configured Workflow Packer Build

--- provisioners/firstrun.sh
+++ provisioners/firstrun.sh
@@ -21,8 +21,6 @@ KBEOF
    dpkg-reconfigure -f noninteractive keyboard-configuration
 fi
 
-chown -R ${USER}:${USER} /opt/ansible-venv
-
 # --- Cleanup ---
 rm -f /boot/firstrun.sh
 sed -i 's| systemd.run.*||g' /boot/cmdline.txt
